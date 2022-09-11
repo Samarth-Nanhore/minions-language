205c6621701fbfe2661d5f01b13b0197d8352cf6
samarth <84143644+Samarth-Nanhore@users.noreply.github.com>
2022-09-11 18:03:49 +0530

    minions API attached

---
+++ minions.js
@@ -0,0 +1,35 @@
+//query selector stored in variable
+var btn = document.querySelector("#btn-translate");
+var user = document.querySelector("#userText");
+var result = document.querySelector("#output");
+
+//API 
+var serverAPI = "https://api.funtranslations.com/translate/minion.json"
+
+// provide input to API server
+function getTranslationURL(engtext) {
+    return serverAPI + "?" + "text=" + engtext
+}
+
+// if the error occur
+function errorHandler(error) {
+    console.log("error occured", error);
+    alert("Oops server error! try again after some time")
+}
+
+// processing: fetching translated data from server to the client browser
+function clickHandler() {
+    var inputText = user.value;
+
+    // calling server for processing
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            result.innerText = translatedText; // output
+        })
+        .catch(errorHandler)
+};
+
+// initiate server call button
+btn.addEventListener("click", clickHandler)
